5378fd817cea002887d491b8d5a9603df1692c99
Kathryn Icuspit <60863018+kathrynicuspit@users.noreply.github.com>
2024-05-24 01:02:59 +0000

    Change name take 2

--- laravel-docker/resources/views/welcome.blade.php
+++ laravel-docker/resources/views/welcome.blade.php
@@ -90,6 +90,7 @@
                                         </div>
 
                                         <div class="pt-3 sm:pt-5 lg:pt-0">
+                                            <h2 class="text-xl font-semibold text-black dark:text-white">Documentation</h2>
                                             <h2 class="text-xl font-semibold text-black dark:text-white">Kathryn Ann Icuspit</h2>
 
                                             <p class="mt-4 text-sm/relaxed">
